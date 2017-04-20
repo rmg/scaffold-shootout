c603e41a31d6929e87343776fa5cf15220b38631
Ryan Graham <r.m.graham@gmail.com>
2017-04-20 12:23:39 -0700

    phoenix_blog: manually add /posts resource route

--- phoenix_blog/web/router.ex
+++ phoenix_blog/web/router.ex
@@ -19,6 +19,7 @@ defmodule PhoenixBlog.Router do
     get "/", PageController, :index
 
     resources "/users", UserController
+    resources "/posts", PostController
   end
 
   # Other scopes may use custom stacks.
