49435d98c5d5be2b787f469dc72f88d038d0e69a
Ryan Graham <r.m.graham@gmail.com>
2017-04-20 13:45:25 -0700

    sails generate controller Comment create show index update delete

---
+++ sails-blog/api/controllers/CommentController.js
@@ -0,0 +1,61 @@
+/**
+ * CommentController
+ *
+ * @description :: Server-side logic for managing Comments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `CommentController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CommentController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CommentController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CommentController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CommentController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
+
