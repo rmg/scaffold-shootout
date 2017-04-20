ee3d76166d858c22648225571b562bfd74c8c95c
Ryan Graham <r.m.graham@gmail.com>
2017-04-20 13:44:43 -0700

    sails generate controller Post create show index update delete

---
+++ sails-blog/api/controllers/PostController.js
@@ -0,0 +1,61 @@
+/**
+ * PostController
+ *
+ * @description :: Server-side logic for managing Posts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `PostController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
+
