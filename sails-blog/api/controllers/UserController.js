a78549e07f1eeb1bac0e6fec99301b9664c3869a
Ryan Graham <r.m.graham@gmail.com>
2017-04-20 13:38:55 -0700

    sails generate controller User create show index update delete

---
+++ sails-blog/api/controllers/UserController.js
@@ -0,0 +1,61 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
+
